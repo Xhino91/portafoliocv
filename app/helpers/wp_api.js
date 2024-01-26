b1f151ac218cec2ce53b4c9c5cddded7c8efd821
Xhino91 <gary.max@hotmail.com>
2024-01-26 08:13:53 -0600

    update db

--- app/helpers/wp_api.js
+++ app/helpers/wp_api.js
@@ -1,7 +1,7 @@
 
  //https://developer.wordpress.org/rest-api/
 
- const API_WP = `https://ontime-3d06f-default-rtdb.firebaseio.com/`,
+ const API_WP = `https://ontime-2956b-default-rtdb.firebaseio.com/`,
  USERS = `${API_WP}/users.json`,
  ITEMS = `${API_WP}/items`,
  SUBITEMS = `${API_WP}/subitem`,
