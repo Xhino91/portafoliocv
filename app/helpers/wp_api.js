b472937d1b042545287851856d51b18903411bc5
Xhino91 <102310316+difusiongr@users.noreply.github.com>
2024-06-10 15:18:08 -0600

    Update wp_api.js

--- app/helpers/wp_api.js
+++ app/helpers/wp_api.js
@@ -1,14 +1,14 @@
 //https://developer.wordpress.org/rest-api/ https://ontime-2956b-default-rtdb.firebaseio.com/
 
-const API_KEY = "AIzaSyC2SuyT9yA3cxFbz2uLf0YLqhJ_uoLKKOc",
-  AUTH_DOMAIN = "ontime-4a320.firebaseapp.com",
-  DB_URL = "https://ontime-4a320-default-rtdb.firebaseio.com",
-  PROJECT_ID = "ontime-4a320",
-  STORAGE_BUCKET = "ontime-4a320.appspot.com",
-  MESSAGIN_SENDERING_ID = "482538679294",
-  APP_ID = "1:482538679294:web:d80a0fb423066f4f1feeaf",
+const API_KEY = "AIzaSyBWjS2VrmW7eFUd34YL13VACi_6OX9Q7VI",
+  AUTH_DOMAIN = "ontime-2956b.firebaseapp.com",
+  DB_URL = "https://ontime-2956b-default-rtdb.firebaseio.com",
+  PROJECT_ID = "ontime-2956b",
+  STORAGE_BUCKET = "ontime-2956b.appspot.com",
+  MESSAGIN_SENDERING_ID = "7596898763",
+  APP_ID = "1:7596898763:web:2e32c7bf836d72846c9e4e",
   USER = "proyectosintlogis@gmail.com",
-  PASS = "Project2024%";
+  PASS = "Ontime2024%";
 
 export default {
   API_KEY,
